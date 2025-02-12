cb7b54233d93645052fefabb3ccb063ff473b7aa
tdogcodes <trxcycsgo@gmail.com>
2025-02-11 16:39:53 -0800

    Hero section

--- prescripto/src/components/Navbar.jsx
+++ prescripto/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets_frontend/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
     const navigate =  useNavigate();
+    const [showMenu, setShowMenu ] = useState(false);
+    const [token, setToken ] = useState(true);
 
   return (
     <nav className='flex justify-between items-center text-sm py-4 mb-5 border-b border-b-gray-400'>
@@ -28,8 +30,21 @@ const Navbar = () => {
             </NavLink>
         </ul>
         <div className='flex items-center gap-4'>
+            {token ? 
+            <div className='flex items-center gap-2 cursor-pointer group relative'>
+                <img className='rounded-full w-8' src={assets.profile_pic} alt="profile pic" />
+                <img className='w-2.5' src={assets.dropdown_icon} alt="profile pic" />
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium hidden group-hover:block text-gray-600 '>
+                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                        <p onClick={()=>navigate('/my-profile')} className="hover:text-black cursor-pointer">My Profile</p>
+                        <p onClick={()=>navigate('/my-appointments')} className="hover:text-black cursor-pointer">My Appointments</p>
+                        <p onClick={()=>setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
+                    </div>
+                </div>
+            </div> :
             <button onClick={()=>navigate('/login')} className='bg-[var(--primary)] text-white px-8 py-3 rounded-full font-light 
             hidden md:block cursor-pointer'>Create account</button>
+            }
         </div>
     </nav>
   )
---
+++ prescripto/src/components/SpecialityMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div>SpecialityMenu</div>
+  )
+}
+
+export default SpecialityMenu
