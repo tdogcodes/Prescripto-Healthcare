9126718948cb8974cc07bfb6be3c871698e664a1
tdogcodes <trxcycsgo@gmail.com>
2025-04-13 22:01:48 -0700

    updated footer

--- prescripto/src/components/Footer.jsx
+++ prescripto/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
         {/* Copyright Text */}
         <div>
             <hr />
-            <p className='py-5 text-sm text-center '>Copyright 2024 @ Greatstack.dev - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center '>Copyright 2024 @ trxcycsgo@gmail.com - All Right Reserved.</p>
         </div>
     </div>
   )
